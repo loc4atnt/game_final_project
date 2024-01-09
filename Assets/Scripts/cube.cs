454b5f05a6896337b3074f5d44f3f473b889f673
Lê Hải Duy <63355568+duylehai@users.noreply.github.com>
2024-01-09 10:30:31 +0700

    commit

---
+++ Assets/Scripts/cube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    public GameObject obj;
+    public Material inEffect;
+    Material dafaultEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dafaultEffect = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("INN");
+        GetComponent<MeshRenderer>().material = inEffect;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("OUTTT");
+        GetComponent<MeshRenderer>().material = dafaultEffect;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        print("stayyy");
+    }
+}
